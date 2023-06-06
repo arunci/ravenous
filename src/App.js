923c8be5b82940141a84a5fb0599ae8159204d1b
javier <javier@arunci.com>
2023-06-06 13:44:40 +0200

    change app message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Codecademy!
+          This is my first react app!
         </p>
         <a
           className="App-link"
