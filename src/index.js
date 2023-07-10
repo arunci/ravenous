a26f42803e330b9dfc6955a8487392a00dd27dd5
arunci <96084440+arunci@users.noreply.github.com>
2023-07-11 01:05:17 +0200

    add error message

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/app/app";
 
-const root = createRoot(document.getElementById("app"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const appElement = document.getElementById("app");
+
+if (!appElement) {
+    console.error("Could not find the element with id 'app'. Make sure the element exists in the HTML.");
+} else {
+    const root = createRoot(appElement);
+    root.render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+}
