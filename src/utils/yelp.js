8430011757700736600ff8697738e38483b1c6c5
arunci <96084440+arunci@users.noreply.github.com>
2023-07-19 13:59:39 +0200

    make asynchronous function

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -1,38 +1,26 @@
-const yelp =
+const apiKey =
   "4mGZXiy14_HLpxFBzkuVZAak8_TdMtoeMvekkC6jETNajFcn-vCixcFoKk68UojfIBqon9Vc_9DYvOy0Tn1qB4oI8j8frun-ssVQIZqOtIZpohl3lMP7yQT2eTW3ZHYx";
-// base yelp url
-const baseUrl = "https://api.yelp.com/v3";
-// search business endpoint
-const searchBusinessEndpoint = "/businesses/search";
-// complete path to endpoint
-const finalEndpoint = baseUrl + searchBusinessEndpoint;
 
-const searchBusiness = (term, location, sort_by) => {
-  const config = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${yelp}`,
-      "Content-Type": "application/json",
-    },
-  };
+const searchBusiness = async (term, location, sort_by) => {
+  const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sort_by}&limit=10`;
 
-  const url = new URL(finalEndpoint);
-  const params = {
-    term: term,
-    location: location,
-    sort_by: sort_by,
-    limit: 10,
-  };
-  Object.keys(params).forEach((key) =>
-    url.searchParams.append(key, params[key])
-  );
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-  return fetch(url, config)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.businesses);
-    })
-    .catch((error) => console.error("Error:", error));
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data.businesses);
+  } catch (error) {
+    console.log("Error:", error);
+  }
 };
 
-searchBusiness("Italian", "NYC", "review_count");
+searchBusiness("Italian", "Madrid", "best_match");
