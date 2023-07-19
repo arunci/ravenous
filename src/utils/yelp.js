8b1699a27ecb29deea8e91b720c951ac9491dc10
arunci <96084440+arunci@users.noreply.github.com>
2023-07-19 14:39:00 +0200

    finished api module

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -17,10 +17,29 @@ const searchBusiness = async (term, location, sort_by) => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data.businesses);
+    if (data.businesses) {
+      const businesses = data.businesses
+      // console.log(data.businesses);
+      return businesses.map(business =>
+      {
+        console.log(business)
+        return {
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }
+      })
+    }
+
   } catch (error) {
     console.log("Error:", error);
   }
 };
 
-searchBusiness("Italian", "Madrid", "best_match");
+searchBusiness("Italian", "Madrid", "best_match")
