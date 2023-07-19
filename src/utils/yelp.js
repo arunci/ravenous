3156d820f53c9459f9f7c352ccb565c06d813a43
arunci <96084440+arunci@users.noreply.github.com>
2023-07-19 02:53:32 +0200

    use fetch instead of axios

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -1,33 +1,38 @@
-// import axios from "axios";
-const axios = require("axios");
-require("dotenv").config();
-
-const yelp = process.env.REACT_APP_API_KEY;
+const yelp =
+  "rqSzxKH7M_H7ROGbt994nDKArF2zwWbpRuIRjuS1TumWOOFAeiSjd7I702SosxTjYi_SjjERicYTQMAw04OrB4RgB7dxBOWSYOFPGUaBTrFoyh9tZUGLfk_S9ai2ZHYx";
 // base yelp url
 const baseUrl = "https://api.yelp.com/v3";
 // search business endpoint
 const searchBusinessEndpoint = "/businesses/search";
 // complete path to endpoint
-const finalEndpoint = baseUrl + searchBusinessEndpoint
+const finalEndpoint = baseUrl + searchBusinessEndpoint;
 
 const searchBusiness = (term, location, sort_by) => {
   const config = {
+    method: "GET",
     headers: {
       Authorization: `Bearer ${yelp}`,
-    },
-    params: {
-      term: term,
-      location: location,
-      sort_by: sort_by,
-      limit: 10
+      "Content-Type": "application/json",
     },
   };
 
-  return axios
-    .get(finalEndpoint, config)
-    .then((response) => {
-      console.log(response.data.businesses);
-    });
+  const url = new URL(finalEndpoint);
+  const params = {
+    term: term,
+    location: location,
+    sort_by: sort_by,
+    limit: 10,
+  };
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+
+  return fetch(url, config)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.businesses);
+    })
+    .catch((error) => console.error("Error:", error));
 };
 
 searchBusiness("Italian", "NYC", "review_count");
